32eb6ee13f81a8d018a0e1274f077b7b1352eede
Atakan Tekoğlu <atakantekoglu@192.168.1.108>
2022-12-25 17:19:47 +0300

    handle todo addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
-
-  const [todo, setTodo] = useState<string>("")
-
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const handleAdd = (e: React.FormEvent): void => {
+    e.preventDefault();
+    setTodos([...todos, {id: Date.now(), todo, completed: false}])
+    setTodo("");
+  }
+  
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField 
+        todo={todo} 
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Todo } from '../model';
 import "./styles.css";
 
 interface Props {
     todo: string,
-    setTodo: React.Dispatch<React.SetStateAction<string>>
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void,
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
   return (
-   <form className="input">
-    <input 
+   <form 
+      className="input" 
+      onSubmit={e => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+    <input
+        ref={inputRef}
         type="text" 
         name="todo" 
         placeholder='Enter a todo'
         className='input__box'
+        value={todo}
         onChange={(e) => {setTodo(e.target.value)}}
     />
     <button type="submit" className='input_submit'>
