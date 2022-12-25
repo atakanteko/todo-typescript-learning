eb1b21b094241cd0254c3522b7274c01c34c037c
Atakan Tekoğlu <atakantekoglu@192.168.1.108>
2022-12-25 16:23:08 +0300

    create input text field with interface

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./styles.css";
 
-const InputField = () => {
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
-    <div>
-      
-    </div>
+   <form className="input">
+    <input 
+        type="text" 
+        name="todo" 
+        placeholder='Enter a todo'
+        className='input__box'
+        onChange={(e) => {setTodo(e.target.value)}}
+    />
+    <button type="submit" className='input_submit'>
+        Go
+    </button>
+   </form>
   )
 }
 
